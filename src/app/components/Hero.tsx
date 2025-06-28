309112d4b3cb2a9c456cd0471dab4e1c2fe47958
Kudae Sithu <kudaesithu2@gmail.com>
2025-06-28 21:36:39 +0630

    Update Hero component to link resume download; replace button with anchor tag for improved accessibility.

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -27,12 +27,15 @@ const Hero = () => {
             learn new tools and frameworks to improve my skills.
           </p>
           <div className="flex items-center justify-center md:justify-start space-x-4 font-medium">
-            <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 text-sm font-medium text-white backdrop-blur-3xl">
-                Download Resume
-              </span>
-            </button>
+            <a href="/Kudae-Sithu-Resume.pdf" download>
+              <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px]">
+                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 text-sm font-medium text-white backdrop-blur-3xl">
+                  Download Resume
+                </span>
+              </button>
+            </a>
+
             <a
               href="#work"
               className="flex justify-center items-center gap-2 cursor-pointer"
