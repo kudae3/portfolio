dffb9c084eea1bce21222d8dac8cf91dc940b850
Kudae Sithu <kudaesithu2@gmail.com>
2025-07-04 22:49:21 +0630

    Update text size in ContactMe component for improved readability

--- src/app/components/ContactMe.tsx
+++ src/app/components/ContactMe.tsx
@@ -42,7 +42,7 @@ const ContactMe = () => {
         <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">
           Contact Me
         </h2>
-        <p className="text-gray-300 text-xs md:text-[14px] max-w-2xl mx-auto mb-6">
+        <p className="text-gray-300 text-xs md:text-[16px] max-w-2xl mx-auto mb-6">
           Have a question, proposal, or just want to say hello? Fill out the
           form below and I&apos;ll get back to you soon!
         </p>
