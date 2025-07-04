4c224bd909b4aa43d8e736baa5178fbb464d39c2
Kudae Sithu <kudaesithu2@gmail.com>
2025-07-04 15:19:15 +0630

    Refactor Certificates component to standardize height and improve text styling

--- src/app/components/Certificates.tsx
+++ src/app/components/Certificates.tsx
@@ -11,12 +11,12 @@ const Certificates = () => {
       </h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         <AnimatedContent>
-          <div className="bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-start py-[16px] px-[26px] h-[98px] md:h-[110px] lg:h-[120px] rounded-[20px]">
+          <div className="bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-start py-[16px] px-[26px] h-[90px] md:h-[110px] lg:h-[120px] rounded-[20px]">
             <h4 className="text-sm md:text-[15px] lg:text-lg font-semibold mb-2">
               Introdution to Front-end Development
             </h4>
-            <div className="text-gray-300 text-xs">
-              <div className="flex justify-start items-center gap-1 text-blue-500 font-extrabold text-xs ">
+            <div className="text-gray-300 text-xs md:text-[13px]">
+              <div className="flex justify-start items-center gap-1 text-blue-500 font-extrabold ">
                 <FaMeta className="block md:hidden lg:block" />
                 <p>Meta</p>
               </div>
@@ -25,12 +25,12 @@ const Certificates = () => {
         </AnimatedContent>
 
         <AnimatedContent>
-          <div className="bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-start py-[16px] px-[26px] h-[98px] md:h-[110px] lg:h-[120px] rounded-[20px]">
+          <div className="bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-start py-[16px] px-[26px] h-[90px] md:h-[110px] lg:h-[120px] rounded-[20px]">
             <h4 className="text-sm md:text-[15px] lg:text-lg font-semibold mb-2">
               Python Data Structures
             </h4>
-            <div className="text-xs">
-              <div className="flex justify-start items-center gap-1 text-yellow-600 font-extrabold text-xs">
+            <div className="text-xs md:text-[13px]">
+              <div className="flex justify-start items-center gap-1 text-yellow-600 font-extrabold">
                 <FaUniversity className="block md:hidden lg:block" />
                 <p>University of Michigan</p>
               </div>
@@ -39,12 +39,12 @@ const Certificates = () => {
         </AnimatedContent>
 
         <AnimatedContent>
-          <div className="bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-start py-[16px] px-[26px] h-[98px] md:h-[110px] lg:h-[120px] rounded-[20px]">
+          <div className="bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-start py-[16px] px-[26px] h-[90px] md:h-[110px] lg:h-[120px] rounded-[20px]">
             <h4 className="text-sm md:text-[15px] lg:text-lg font-semibold mb-2">
               Diploma In Software Engineering
             </h4>
-            <div className="text-gray-300 text-xs">
-              <div className="flex justify-start items-center gap-1 text-green-500 font-extrabold text-xs">
+            <div className="text-gray-300 text-xs md:text-[13px]">
+              <div className="flex justify-start items-center gap-1 text-green-500 font-extrabold">
                 <FaUniversity className="block md:hidden lg:block" />
                 <p>European Open University</p>
               </div>
